94b9183a6a17d553818355f0ad01feaac50e4bc5
Grant Timmerman <granttimmerman@gmail.com>
2013-05-26 15:00:46 -0700

    added alerts

--- prototype/scripts/script.js
+++ prototype/scripts/script.js
@@ -19,50 +19,6 @@ var maxNumRows = 10;
 var vidWallMinWidth = 400;
 var vidWallMinHeight = 250;
 
-// Update a particular HTML element with a new value
-function updateHTML(elmId, value) {
-	document.getElementById(elmId).innerHTML = value;
-}
-
-// This function is called when an error is thrown by the player
-function onPlayerError(errorCode) {
-	alert("An error occured of type:" + errorCode);
-}
-
-// This function is called when the player changes state
-function onPlayerStateChange(newState) {
-	updateHTML("playerState", newState);
-}
-
-// This function is called when the player changes state
-function onPlayerStateChange2(newState) {
-	updateHTML("playerState2", newState);
-}
-
-// Display information about the current state of the player
-function updatePlayerInfo() {
-	// Also check that at least one function exists since when IE unloads the
-	// page, it will destroy the SWF before clearing the interval.
-	if(ytplayer && ytplayer.getDuration) {
-		updateHTML("videoDuration", ytplayer.getDuration());
-		updateHTML("videoCurrentTime", ytplayer.getCurrentTime());
-		updateHTML("bytesTotal", ytplayer.getVideoBytesTotal());
-		updateHTML("startBytes", ytplayer.getVideoStartBytes());
-		updateHTML("bytesLoaded", ytplayer.getVideoBytesLoaded());
-	}
-}
-
-// Display information about the current state of the player
-function updatePlayerInfo2() {
-	if(ytplayer2 && ytplayer2.getDuration) {
-		updateHTML("videoDuration2", ytplayer2.getDuration());
-		updateHTML("videoCurrentTime2", ytplayer2.getCurrentTime());
-		updateHTML("bytesTotal2", ytplayer2.getVideoBytesTotal());
-		updateHTML("startBytes2", ytplayer2.getVideoStartBytes());
-		updateHTML("bytesLoaded2", ytplayer2.getVideoBytesLoaded());
-	}
-}
-
 // This function is automatically called by the player once it loads
 function onYouTubePlayerReady(playerId) {
     var player = document.getElementById(playerId);
@@ -171,9 +127,9 @@ function rescaleVideos () {
 function setupQuery () {
     var query = window.location.hash;
     if (!query) {
-        alert('Type #topic at end of url for search.');
         query = '#psy';
         window.location.hash = query;
+        alertify.error('Type a query in the serch bar (where "psy" is)');
     }
     $('#videoSearch').val(query.substring(1));
     loadFromQuery(query.substring(1));
@@ -211,7 +167,6 @@ function _run () {
             case 63:
                 $('#videoSearch').focus().select();
                 return false;
-                break;
         }
     });
 
